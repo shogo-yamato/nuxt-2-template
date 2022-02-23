314f2d000d42f814a22dea65f13f0a8960dac128
shogo-yamato <74167112+shogo-yamato@users.noreply.github.com>
2022-02-23 19:23:03 +0900

    feat: add dependencies for scss and stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,28 @@
 module.exports = {
   customSyntax: 'postcss-html',
   extends: [
+    // NOTE:
+    // stylelint-config-standardは
+    // create-nuxt-appで設定された内容です
+    // 生CSSは一切書かないのであれば
+    // 依存関係/configから消してもいいと思います
     'stylelint-config-standard',
     'stylelint-config-recommended-vue',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-recess-order',
+    'stylelint-rscss/config',
     'stylelint-config-prettier',
   ],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    /* {}内のnestは1回まで。 擬似クラスと@includeはネストとカウントしない */
+    'max-nesting-depth': [
+      2,
+      {
+        ignore: ['pseudo-classes'],
+        ignoreAtRules: ['include'],
+      },
+    ],
+  },
 }
